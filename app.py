0eb7072d248845e02dbe5181d32515923a09ddb5
OsamaAbdulKhalique <osama.abdulkhalique@e-butler.com>
2024-03-24 22:16:27 +0300

    Added GPT Feedback and slight changes to the score.html file

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-# Necessary Libraries
 from flask import Flask, render_template, request
-from transformers import AutoTokenizer, T5ForConditionalGeneration
-import numpy as np
 import pandas as pd
 import joblib
+from openai import OpenAI
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
 
-# Load the models and tokenizer
-grammarly_tokenizer = AutoTokenizer.from_pretrained("grammarly/coedit-large")
-grammarly_model = T5ForConditionalGeneration.from_pretrained("grammarly/coedit-large")
+
+client = OpenAI(api_key="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")
 
 bilstm_tokenizer = Tokenizer()
 bilstm_tokenizer.fit_on_texts(pd.read_csv("data/train.csv"))
@@ -44,19 +41,30 @@ def score():
         
         # Process input_text using the second model (replace this with your actual second model)
 
-        return render_template('score.html', input_text=input_text, prediction=prediction[0], second_model_prediction=second_model_prediction[0])
+        return render_template('score.html', input_text=input_text, 
+                               prediction=prediction[0], 
+                               second_model_prediction=second_model_prediction[0]
+                              )
 
 @app.route('/process_text', methods=['POST'])
 def process_text():
     if request.method == 'POST':
         input_text = request.form['input_text']
 
-        # Process input_text using the Grammarly model
-        input_ids_t5 = grammarly_tokenizer(input_text, return_tensors="pt").input_ids
-        output_ids_t5 = grammarly_model.generate(input_ids_t5)
-        output_text_t5 = grammarly_tokenizer.decode(output_ids_t5[0], skip_special_tokens=True)
-
-        return render_template('process_text.html', input_text=input_text, output_text_t5=output_text_t5)
+        prompt = "Correct the grammar of  the following essay: "+ input_text 
+        completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo-1106",
+            response_format={"type": "text"},
+        )
+        corrected_essay = completion.choices[0].message.content.replace("\n", "")
+        # here 
+        return render_template('process_text.html', input_text=input_text , corrected_essay=corrected_essay)
 
 @app.route('/error', methods=['GET'])
 def error():
